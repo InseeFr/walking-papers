93b8aaea72ddd2b126a97ef0439dc2862a5544c3
Laurent Caouissin <caouissin.laurent@gmail.com>
2026-01-12 15:09:26 +0100

    build: add vite-envs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,13 @@ export default function Header() {
   const { t } = useTranslation()
 
   return (
-    <div className="h-16 bg-gray-800 border-b content-center text-center">
-      <span className="font-bold text-xl text-white">
+    <div className="h-16 bg-gray-800 border-b relative flex items-center justify-end px-4">
+      <span className="font-bold text-xl text-white absolute left-1/2 transform -translate-x-1/2">
         {t('common.appName')}
+      </span>
+      <span className="text-white italic">
+        Version {import.meta.env.APP_VERSION} | Lunatic{' '}
+        {import.meta.env.LUNATIC_VERSION.replace('^', '')}
       </span>
     </div>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { oidcSpa } from 'oidc-spa/vite-plugin'
 import { defineConfig } from 'vite'
+import { viteEnvs } from 'vite-envs'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
@@ -18,5 +19,19 @@ export default defineConfig({
       enableTokenExfiltrationDefense: true,
     }),
     tailwindcss(),
+    viteEnvs({
+      computedEnv: async () => {
+        const path = await import('node:path')
+        const fs = await import('node:fs/promises')
+
+        const packageJson = JSON.parse(
+          await fs.readFile(path.resolve(__dirname, 'package.json'), 'utf-8'),
+        )
+        return {
+          APP_VERSION: packageJson.version ?? '',
+          LUNATIC_VERSION: packageJson.dependencies['@inseefr/lunatic'] ?? '',
+        }
+      },
+    }),
   ],
 })
